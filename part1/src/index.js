b7e23e0d539c2b1328032d6345aa476acd37dc9d
Fabio Gomes <fabiolpgomes@hotmail.com>
2022-11-07 19:23:41 +0100

    React part1 App and index

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,5 +2,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'))
+//ReactDOM.render(<App />, document.getElementById('root'))
 
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
